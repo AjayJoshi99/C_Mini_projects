1a4efbe2b46247dfe0a20e758852a83725f7c3c6
Ajay Joshi <ajayjoshi1908@gmail.com>
2023-08-27 15:09:20 +0530

    Add files via upload

---
+++ clock.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<windows.h>
+
+ int  main()
+ { 
+  int h, m, s;
+  int d = 1; 
+  
+  printf("set time (hh:mm:ss) : \n");
+  scanf("%d:%d:%d", &h, &m, &s);
+
+  if(h>12 || m>60 || s>60 )
+        { printf("error!\n");
+            exit(0);}
+  
+  while(1)
+  { 
+    s++;
+
+    if(s>59)
+        {
+          m++;
+          s=0;
+        }
+        if(m>59)
+        { 
+          h++;
+          m=0;
+        }
+    if(h>12)
+        { 
+          h=1;
+        }
+  
+  system("cls");
+  
+  printf(" \n clock : ");
+  printf("\n %02d: %02d: %02d", h,m,s);
+  sleep(d);
+  }
+
+  
+ }
+ 
